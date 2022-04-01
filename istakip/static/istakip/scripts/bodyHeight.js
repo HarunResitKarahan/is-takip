2629aab3affd340be6bb71a9857473db5174607a
Harun <harun_artin@hotmail.com>
2022-04-02 00:58:33 +0300

    .

--- istakip/static/istakip/scripts/bodyHeight.js
+++ istakip/static/istakip/scripts/bodyHeight.js
@@ -5,13 +5,13 @@
 // console.log($('.input').innerWidth())
 
 if (localStorage.getItem('button') !== null) {
-    $(localStorage.getItem('button')).css({display: 'flex'})
+    document.getElementById(localStorage.getItem('button')).style.display = 'flex'
 }
 // $(localStorage.getItem('button')).css({display: flex})
 // console.log(localStorage.getItem('button'))
 
 $('.buttons#1').click(function( event ){ // <---- "event" parameter here
-    localStorage.setItem('button', '.buttons#1')
+    localStorage.setItem('button', '1')
     document.querySelector('#kayit').style.display = 'flex'
     var width = $('#1.register').outerWidth()
     $('#2.register').css({width: width})
@@ -23,11 +23,11 @@ $('.buttons#1').click(function( event ){ // <---- "event" parameter here
     $('.worker').css({height: toplam})
 });
 $('.buttons#2').click(function( event ){ // <---- "event" parameter here
-    localStorage.setItem('button', '.buttons#2')
+    localStorage.setItem('button', '2')
     document.querySelector('#takip').style.display = 'flex'
 });
 $('.buttons#3').click(function( event ){ // <---- "event" parameter here
-    localStorage.setItem('button', '.buttons#3')
+    localStorage.setItem('button', '3')
     document.querySelector('#ayarlar').style.display = 'flex'
 
     var toplam = 0
@@ -37,7 +37,7 @@ $('.buttons#3').click(function( event ){ // <---- "event" parameter here
     $('#ayarlar .worker').css({height: toplam})
 });
 $('.buttons#4').click(function( event ){ // <---- "event" parameter here
-    localStorage.setItem('button', '.buttons#4')
+    localStorage.setItem('button', '4')
     document.querySelector('#rapor').style.display = 'flex'
 
     var toplam = 0
